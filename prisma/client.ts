31a5578463f42d509ac12ef8ea18f14ab3be6d5a
Vikas <vikas@evenforce.com>
2024-11-03 13:55:21 +0530

    clients

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,19 +1,40 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: prismaClientSingleton | undefined
+// }
+
+
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient();
+};
+
+type PrismaClientSingletonType = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as typeof globalThis & {
+  prisma?: PrismaClientSingletonType;
+};
+
+// Check if there's already a Prisma instance in the global object; otherwise, create a new one
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
 
-type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: prismaClientSingleton | undefined
-}
-
-
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export default prisma;
